5f674c91133216cad3f4730ca31d6e9c2a1e9e34
Raja28 <rajadavid03@gmail.com>
2024-09-05 19:17:39 +0530

    mongodb url add

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 exports.connect = async () => {
     try {
-        const connection = mongoose.connect(process.env.MONGODB_URL);
+        const connection = mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017");
         if (connection) {
             console.log("DB connected successfully");
         }
